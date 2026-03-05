513a845cc604e29827ef2d0eadb85f82f3f81eb2
Gavin Sacek-Brooks <gsacekbr@charlotte.edu>
2026-03-05 03:02:08 -0500

    Repo reset

--- Source/Showdown/Showdown.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Showdown.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Showdown, "Showdown" );
-
-DEFINE_LOG_CATEGORY(LogShowdown)
